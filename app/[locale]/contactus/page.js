7021699c178924783d0cfa8bb3da368e7e0cd242
akmisme <aungkaungmyat2318@gmail.com>
2025-11-28 17:04:03 +0630

    akm

--- app/[locale]/contactus/page.js
+++ app/[locale]/contactus/page.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-export default function ContactUs(){
-    return(
-        <section className="mt-30">
-            <div className="">
-                
-            </div>
-        </section>
-    );
+
+export default function ContactUs() {
+  return (
+    <section className="mt-30">
+      <div
+        className="relative h-[70vh] bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: "url('/assets/bagan.jpg')" }}
+      >
+        <p>Hello</p>
+      </div>
+    </section>
+  );
 }
---
+++ app/components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Contact = ()=>{
+    return(
+        <section className="">
+            <div className="">
+                <form className="">
+                    <label></label>
+                    <input type="" placeholder=""/>
+                    <label></label>
+                    <input type="" placeholder=""/>
+                </form>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
